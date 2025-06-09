c9dc447d18722e2616ca9d3a0442459d4dcf3532
Mystic2 <nickwendt7@gmail.com>
2025-06-08 19:11:01 -0700

    cookin

---
+++ routes/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const User = require("../schema/Users.js")
+require('dotenv').config();
+
+
+const connectDB = async () => {
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected!")
+}
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const connectDB = require('./db');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const connectDB = require('./routes/db');
 
 const app = express();
 
-connectDB(); // Connect to MongoDB before starting the server
+app.set('view engine', 'ejs')
+app.use(express.static("views"))
 
-app.get('/', (req, res) => {
-  res.send('Hello from your MongoDB-connected app!');
-});
+app.get("/", (req, res) => { //request and response
+  console.log('Here')
+  res.render('index', { text: "World"})
+}) 
+
+const userRouter = require("./routes/users")
+const dbRouter = require("./routes/db")
+connectDB();
+
+app.use("/db", dbRouter)
+app.use("/users", userRouter)
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server is running on port 3000");
 });
